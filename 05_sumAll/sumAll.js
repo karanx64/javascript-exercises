29f216b199ee626d90860c6fe5ef6e9e29ed70d3
karanx64 <karansinghx64@gmail.com>
2024-12-17 22:24:02 +0530

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (typeof num1 == "number" && typeof num2 == "number") {
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+      if (num1 > 0 && num2 > 0) {
+        let sum = 0;
+        if (num1 < num2) {
+          for (let i = num1; i <= num2; i++) {
+            sum += i;
+          }
+        } else {
+          if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+              sum += i;
+            }
+          }
+        }
+        return sum;
+      } else {
+        return "ERROR";
+      }
+    } else {
+      return "ERROR";
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
